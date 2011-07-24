0c93dfc78c2405bbb26e660268177cef479b0669
sswen4sa <sswen4sa@localhost>
2011-07-24 22:49:05 +0000

    playing around with the grammer, and quasi "type checking"

--- Calc.Backend/Evaluator.fs
+++ Calc.Backend/Evaluator.fs
@@ -6,21 +6,33 @@ open Ast
 open Lexer
 open Parser
 
+let (|Box|) x = box x
+let (|Float|) x = float x
+
 let eval str =
     let rec eval exp =
         match exp with
-        | Rational x -> x
-        | UMinus x -> - eval x
-        | Plus(x,y) -> eval x + eval y
-        | Minus(x,y) -> eval x - eval y
-        | Div(x,y) -> eval x /  eval y
-        | Times(x,y) -> eval x * eval y
-        | Pow(x,y) -> eval x ** eval y
+        | Value x -> x
+        | UMinus(x) -> 
+            match eval x with
+            | Integer x -> Integer(-x)
+            | Rational x -> Rational(-x)
+        | Plus(x,y) -> callCoercedBinop x y (+) (+)
+        | Minus(x,y) -> callCoercedBinop x y (-) (-) 
+        | Div(x,y) -> callCoercedBinop x y (/) (/) 
+        | Times(x,y) -> callCoercedBinop x y (*) (*)
+        | Pow(x,y) -> callCoercedBinop x y ( ** ) ( ** )
         | Fact(n) -> 
-            let rec fact n = 
-                if n=0 then 1 else n * fact(n - 1)
-            fact (eval n |> int) |> float
-
+            match eval n with
+            | Integer(n) ->
+                let rec fact n = 
+                    if n=0 then 1 else n * fact(n - 1)
+                Integer(fact n)
+            | _ -> failwith "factorial is only valid on integers"
+    and callCoercedBinop x y onInteger onRational =
+        match eval x, eval y with
+        | Integer x, Integer y -> Integer(x + y)
+        | (Rational(x) | Integer(Float(x))), (Integer(Float(y)) | Rational(y)) -> Rational(x + y)
     
     let lexbuff = LexBuffer<char>.FromString(str)
     let exp = Parser.start Lexer.tokenize lexbuff
