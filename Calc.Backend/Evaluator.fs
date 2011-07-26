692bb38b2ba1f5c13f616611bd7da4316e5d346f
sswen4sa <sswen4sa@localhost>
2011-07-26 04:55:38 +0000

    working on IL generation

--- Calc.Backend/Evaluator.fs
+++ Calc.Backend/Evaluator.fs
@@ -40,3 +40,61 @@ let eval str =
     let lexbuff = LexBuffer<char>.FromString(str)
     let exp = Parser.start Lexer.tokenize lexbuff
     eval exp
+
+open System.Reflection.Emit
+
+let eval_il str =
+    let dm = System.Reflection.Emit.DynamicMethod("eval", typeof<int>, null)
+    let il = dm.GetILGenerator()
+
+    let rec emit exp =
+        match exp with
+        | Value(Integer x) -> 
+            let code = OpCodes.Ldc_I4
+            Console.WriteLine code
+            il.Emit(code, x)
+        | Value(Rational x) -> 
+            let code = OpCodes.Ldc_R8
+            Console.WriteLine code
+            il.Emit(code, x)
+        | UMinus(x) -> 
+            emit x; 
+            let code = OpCodes.Neg
+            Console.WriteLine code
+            il.Emit(code)
+//        | Plus(x,y) -> callCoercedBinop x y (+) (+)
+//        | Minus(x,y) -> callCoercedBinop x y (-) (-) 
+//        | Div(x,y) -> callCoercedBinop x y (/) (/) 
+//        | Times(x,y) -> callCoercedBinop x y (*) (*)
+//        | Pow(x,y) -> callCoercedBinop x y ( integerPow ) ( ** )
+//        | Fact(n) -> 
+//            match eval n with
+//            | Integer(n) ->
+//                let rec fact n = 
+//                    if n=0 then 1 else n * fact(n - 1)
+//                Integer(fact n)
+//            | _ -> failwith "factorial is only valid on integers"
+//    and callCoercedBinop x y onInteger onRational =
+//        match eval x, eval y with
+//        | Integer x, Integer y -> Integer(onInteger x y)
+//        | (Rational(x) | Integer(Float(x))), (Integer(Float(y)) | Rational(y)) -> Rational(onRational x y)
+    
+    let lexbuff = LexBuffer<char>.FromString(str)
+    let exp = Parser.start Lexer.tokenize lexbuff
+    
+    emit exp |> ignore
+    
+//    let code = OpCodes.Box
+//    Console.WriteLine code
+//    il.Emit(code)
+    
+    let code = OpCodes.Ret
+    Console.WriteLine code
+    il.Emit(code)    
+
+    let d = dm.CreateDelegate(typeof<System.Func<int>>) :?> System.Func<int>
+    Integer(d.Invoke())
+    
+//    match d.Invoke() with
+//    | :? float as x -> Rational(x)
+//    | :? int as x -> Integer(x)
