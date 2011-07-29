70181b7e2eec0b8fda41ce62eb95f334e9baddd0
sswen4sa <sswen4sa@localhost>
2011-07-29 05:01:51 +0000

    Separated symantic analysis from op code emit

--- Calc.Backend/Compiler.fs
+++ Calc.Backend/Compiler.fs
@@ -12,63 +12,44 @@ open  System.Reflection.Emit
 let parseFromString code =
     let lexbuff = LexBuffer<char>.FromString(code)
     let ast = Parser.start Lexer.tokenize lexbuff
-    let rec fillTys = function
-        | Value _ -> ()
-        | Div(x,y,ty) | Times(x,y,ty) | Plus(x,y,ty) | Minus(x,y,ty) | Pow(x,y,ty) ->
-            fillTys x ; fillTys y
-            if x.Type = typeof<float> || y.Type = typeof<float> then
-                ty := typeof<float>
-            else
-                ty := typeof<int>
-        | Fact x -> 
-            fillTys x
-            if x.Type <> typeof<int> then
-                failwithf "invalid type in factorial (must be int but is: %A)" x.Type
-        | UMinus (x,ty) ->
-            fillTys x
-            ty := x.Type
-
-    fillTys ast
     ast
 
 let emitOpCodes (il:ILGenerator) ast =
     let rec emit ast =
         match ast with
-        | Value(Integer x) -> 
+        | Int32(x,_) -> 
             il.Emit(OpCodes.Ldc_I4, x)
-        | Value(Rational x) -> 
+        | Double(x,_) -> 
             il.Emit(OpCodes.Ldc_R8, x)
         | UMinus(x,_) -> 
             emit x
             il.Emit(OpCodes.Neg)
-        | Plus(x,y,ty) -> emitCoercedBinop x y !ty OpCodes.Add
-        | Minus(x,y,ty) -> emitCoercedBinop x y !ty OpCodes.Sub
-        | Div(x,y,ty) -> emitCoercedBinop x y !ty OpCodes.Div
-        | Times(x,y,ty) -> emitCoercedBinop x y !ty OpCodes.Mul
-//        | Fact(n) -> 
-//            emit n
-//            for i in 2 .. n do
-//                emit n
+        | Binop(op,x,y,ty) -> 
+            if ty = typeof<int> then
+                emit x ; emit y
+            else //float
+                emit x
+                if x.Type = typeof<int> then
+                    il.Emit(OpCodes.Conv_R8)
 
-                
+                emit y
+                if y.Type = typeof<int> then
+                    il.Emit(OpCodes.Conv_R8)
+
+            let ilop =
+                match op with
+                | Plus -> OpCodes.Add
+                | Minus -> OpCodes.Sub
+                | Times -> OpCodes.Mul
+                | Div -> OpCodes.Div
+                | Pow -> failwith "pow not implemented"
+
+            il.Emit(ilop)
         | _ -> failwith "not implemented"
-    and emitCoercedBinop x y ty oc =        
-        if ty = typeof<int> then
-            emit x ; emit y
-        else //float
-            emit x
-            if x.Type = typeof<int> then
-                il.Emit(OpCodes.Conv_R8)
-
-            emit y
-            if y.Type = typeof<int> then
-                il.Emit(OpCodes.Conv_R8)
-
-        il.Emit(oc)
 
     emit ast |> ignore
 
-let delegateFromAst (ast:exp) =
+let delegateFromAst ast =
     let dm = System.Reflection.Emit.DynamicMethod("", typeof<obj>, null)
     let il = dm.GetILGenerator()
     emitOpCodes il ast
--- Calc.Frontend/Ast.fs
+++ Calc.Frontend/Ast.fs
@@ -1,28 +1,54 @@
 ﻿module Swensen.Calc.Ast
 open System
 
-type value =
-    | Rational of float
-    | Integer  of int
+type binop =
+    | Plus | Minus | Times | Div | Pow
 
-type exp =
-    | Value    of value
-    | Plus     of exp * exp * (Type ref)
-    | Minus    of exp * exp * (Type ref)
-    | Times    of exp * exp * (Type ref)
-    | Div      of exp * exp * (Type ref)
-    | Pow      of exp * exp * (Type ref)
-    | UMinus   of exp * (Type ref)
-    | Fact     of exp //always integer
-    with
+module UT =
+    type exp =
+        | Double   of float
+        | Int32    of int
+        | Binop    of binop * exp * exp
+        | UMinus   of exp
+        | Fact     of exp
+
+open UT
+
+type typedExp =
+    | Double   of float * Type
+    | Int32    of int * Type
+    | Binop    of binop * typedExp * typedExp * Type
+    | UMinus   of typedExp * Type
+    | Fact     of typedExp * Type
+    with 
         member this.Type =
             match this with
-            | Value(Integer _) -> typeof<int>
-            | Value(Rational _) -> typeof<float>
-            | Plus(_,_,ty) 
-            | Minus(_,_,ty) 
-            | Times(_,_,ty) 
-            | Div(_,_,ty)
-            | Pow(_,_,ty)
-            | UMinus(_,ty) -> !ty
-            | Fact _ -> typeof<int>
+            | Double(_,ty) -> ty
+            | Int32(_,ty)
+            | Binop(_,_,_,ty)
+            | UMinus(_,ty)
+            | Fact(_,ty) -> ty
+
+let rec tycheck exp =
+    match exp with
+    | exp.Double(x) -> Double(x,typeof<float>)
+    | exp.Int32(x) -> Int32(x,typeof<int>)
+    | exp.UMinus(x) ->
+        let x = tycheck x
+        UMinus(x,x.Type)
+    | exp.Fact(x) ->
+        let x = tycheck x
+        if x.Type <> typeof<int> then
+            failwithf "factorial expects int but got: %A" x.Type
+        else
+            Fact(x, x.Type)
+    | exp.Binop(op,x,y) ->
+        let x, y = tycheck x, tycheck y
+        let ty =
+            if x.Type = typeof<int> && y.Type = typeof<int> then
+                typeof<int>
+            elif x.Type = typeof<float> || y.Type = typeof<float> then
+                typeof<float>
+            else
+                failwithf "binop expects float or int args but got lhs=%A, rhs=%A" x.Type y.Type 
+        Binop(op,x,y,ty)
