e0c99e0cb4a93d71e57052ffd2b3b91db7665dae
sswen4sa <sswen4sa@localhost>
2011-08-27 23:16:03 +0000

    working on making compiler exe

---
+++ NewLang.Compiler.Application/Program.fs
@@ -0,0 +1,8 @@
+﻿module Swensen.NewLang.Compiler.Application.Program
+open Swensen.NewLang
+
+[<EntryPoint>]
+let main [|sourceFileName; asmName; asmFileName;|] =
+    Compiler.compile (System.IO.File.ReadAllText sourceFileName |> Compiler.parseFromString) asmName asmFileName
+    0
+
