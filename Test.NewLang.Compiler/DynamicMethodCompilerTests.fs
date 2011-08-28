0eb4b8c9005129744e6581dfe82542748a46024a
sswen4sa <sswen4sa@localhost>
2011-08-28 03:30:27 +0000

    more tests

--- Test.NewLang.Compiler/DynamicMethodCompilerTests.fs
+++ Test.NewLang.Compiler/DynamicMethodCompilerTests.fs
@@ -26,8 +26,68 @@ let ``Double addition`` () =
     test <@ C.eval "3.0 + 3.0" = box 6.0 @>
 
 [<Fact>]
-let ``Lossless coersion of Int32 to Double`` () =
+let ``Lossless coersion of Int32 to Double in addition`` () =
     test <@ C.eval "3.0 + 3" = box 6.0 @>
+
+[<Fact>]
+let ``Int32 subtraction`` () =
+    test <@ C.eval "3 - 3" = box 0 @>
+
+[<Fact>]
+let ``Double subtraction`` () =
+    test <@ C.eval "3.0 - 3.0" = box 0.0 @>
+
+[<Fact>]
+let ``Lossless coersion of Int32 to Double in sub`` () =
+    test <@ C.eval "3.0 - 3" = box 0.0 @>
+
+[<Fact>]
+let ``Int32 mult`` () =
+    test <@ C.eval "3 * 3" = box 9 @>
+
+[<Fact>]
+let ``Double mult`` () =
+    test <@ C.eval "3.0 * 3.0" = box 9.0 @>
+
+[<Fact>]
+let ``Lossless coersion of Int32 to Double in mult`` () =
+    test <@ C.eval "3.0 * 3" = box 9.0 @>
+
+[<Fact>]
+let ``Int32 div`` () =
+    test <@ C.eval "3 / 3" = box 1 @>
+
+[<Fact>]
+let ``Double div`` () =
+    test <@ C.eval "3.0 / 3.0" = box 1.0 @>
+
+[<Fact>]
+let ``Lossless coersion of Int32 to Double in div`` () =
+    test <@ C.eval "3.0 / 3" = box 1.0 @>
+
+[<Fact>]
+let ``Pow on floats only`` () =
+    test <@ C.eval "3.0 ^ 3.0" = box 27.0 @>
+
+[<Fact>]
+let ``Factorial on ints only`` () =
+    test <@ C.eval "4!" = box 24 @>
+
+[<Fact>]
+let ``unary minus int`` () =
+    test <@ C.eval "-3" = box -3 @>
+
+[<Fact>]
+let ``unary minus float`` () =
+    test <@ C.eval "-3.0" = box -3.0 @>
+
+[<Fact>]
+let ``* stronger than +`` () =
+    test <@ C.eval "2 + 3 * 4" = box 14 @>
+
+[<Fact>]
+let ``parenthesis to overcome precedence`` () =
+    test <@ C.eval "(2 + 3) * 4" = box 20 @>
 
 [<Fact>]
 let ``String concat`` () =
