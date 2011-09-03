1552e0b742c937c6a5fb0d87bcc22bd429272509
sswen4sa <sswen4sa@localhost>
2011-09-03 04:26:12 +0000

    test for references by assembly display name

--- Test.NewLang.Compiler/DynamicMethodCompilerTests.fs
+++ Test.NewLang.Compiler/DynamicMethodCompilerTests.fs
@@ -176,3 +176,7 @@ let ``open expression`` () =
 [<Fact>]
 let ``ref relative path dll`` () =
     test <@ C.eval<obj> "ref \"xunit.dll\" in Xunit.Record()" :? Xunit.Record @>
+
+[<Fact>]
+let ``ref assembly display name`` () =
+    test <@ C.eval "ref \"System.Web, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a\" in open System.Web.Mail in SmtpMail.get_SmtpServer()" = "" @>
