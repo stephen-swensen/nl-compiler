6b0ab62be532b6f290fd1341f5c39693f03bcb74
sswen4sa <sswen4sa@localhost>
2011-09-15 08:19:35 +0000

    finished boolean operators: and,or,xor

--- Test.NewLang.Compiler/DynamicMethodCompilerTests.fs
+++ Test.NewLang.Compiler/DynamicMethodCompilerTests.fs
@@ -440,3 +440,51 @@ let ``false and true`` () =
 let ``false and false`` () =
     test <@ C.eval "false and false" = false @>
 
+[<Fact>]
+let ``lhs of or  must be bool`` () =
+    raises<SemanticErrorException> <@ C.eval "3 or true" @>
+
+[<Fact>]
+let ``rhs of or  must be bool`` () =
+    raises<SemanticErrorException> <@ C.eval "true or 3" @>
+
+[<Fact>]
+let ``true or true`` () =
+    test <@ C.eval "true or true" = true @>
+
+[<Fact>]
+let ``true or false`` () =
+    test <@ C.eval "true or false" = true @>
+
+[<Fact>]
+let ``false or true`` () =
+    test <@ C.eval "false or true" = true @>
+
+[<Fact>]
+let ``false or false`` () =
+    test <@ C.eval "false or false" = false @>
+
+[<Fact>]
+let ``lhs of xor  must be bool`` () =
+    raises<SemanticErrorException> <@ C.eval "3 xor true" @>
+
+[<Fact>]
+let ``rhs of xor  must be bool`` () =
+    raises<SemanticErrorException> <@ C.eval "true xor 3" @>
+
+[<Fact>]
+let ``true xor true`` () =
+    test <@ C.eval "true xor true" = false @>
+
+[<Fact>]
+let ``true xor false`` () =
+    test <@ C.eval "true xor false" = true @>
+
+[<Fact>]
+let ``false xor true`` () =
+    test <@ C.eval "false xor true" = true @>
+
+[<Fact>]
+let ``false xor false`` () =
+    test <@ C.eval "false xor false" = false @>
+
