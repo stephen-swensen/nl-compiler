6a953d1a83856a8c1bcc72c74443aac33074fa5f
sswen4sa <sswen4sa@localhost>
2011-09-17 16:02:52 +0000

    begin while loop tests

--- Test.NewLang.Compiler/WhileLoopTests.fs
+++ Test.NewLang.Compiler/WhileLoopTests.fs
@@ -6,3 +6,14 @@ open Swensen.NewLang
 open System.Collections.Generic
 module C = Compiler
 
+[<Fact>]
+let ``break not allowed outside of while loop`` () =
+    raises<SemanticErrorException> <@ C.eval "break()" @>
+
+[<Fact>]
+let ``continue not allowed outside of while loop`` () =
+    raises<SemanticErrorException> <@ C.eval "continue()" @>
+
+[<Fact>]
+let ``simple while loop`` () =
+    test <@ C.eval "x=0 in (while x<5 do x<-x+1); x " = 5 @>
