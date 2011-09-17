2ee015dcaa2850c7792cc460e09e6008b8aa3fe7
sswen4sa <sswen4sa@localhost>
2011-09-17 00:44:36 +0000

    static operator comparison tests

--- Test.NewLang.Compiler/DynamicMethodCompilerTests.fs
+++ Test.NewLang.Compiler/DynamicMethodCompilerTests.fs
@@ -540,3 +540,50 @@ let ``primitive <= true when ==`` () =
 let ``primitive <= true when <`` () =
     test <@ C.eval "-1 <= 0" = true @>
 
+[<Fact>]
+let ``static operator == true`` () =
+    test <@ C.eval "biginteger(1) == biginteger(1)" = true @>
+
+[<Fact>]
+let ``static operator == false`` () =
+    test <@ C.eval "biginteger(1) == biginteger(2)" = false @>
+
+[<Fact>]
+let ``static operator > true`` () =
+    test <@ C.eval "biginteger(1) > biginteger(0)" = true @>
+
+[<Fact>]
+let ``static operator > false`` () =
+    test <@ C.eval "biginteger(1) > biginteger(2)" = false @>
+
+[<Fact>]
+let ``static operator >= false`` () =
+    test <@ C.eval "biginteger(1) >= biginteger(2)" = false @>
+
+[<Fact>]
+let ``static operator >= true when ==`` () =
+    test <@ C.eval "biginteger(1) >= biginteger(1)" = true @>
+
+[<Fact>]
+let ``static operator >= true when >`` () =
+    test <@ C.eval "biginteger(1) >= biginteger(0)" = true @>
+
+[<Fact>]
+let ``static operator < true`` () =
+    test <@ C.eval "biginteger(0) < biginteger(1)" = true @>
+
+[<Fact>]
+let ``static operator < false`` () =
+    test <@ C.eval "biginteger(1) < biginteger(1)" = false @>
+
+[<Fact>]
+let ``static operator <= false`` () =
+    test <@ C.eval "biginteger(3) <= biginteger(2)" = false @>
+
+[<Fact>]
+let ``static operator <= true when ==`` () =
+    test <@ C.eval "biginteger(1) <= biginteger(1)" = true @>
+
+[<Fact>]
+let ``static operator <= true when <`` () =
+    test <@ C.eval "biginteger(-1) <= biginteger(0)" = true @>
