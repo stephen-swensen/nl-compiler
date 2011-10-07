7efe48b9c60c24bd4624981171a3153150d993a1
stephen swensen <stephen.swensen@gmail.com>
2011-10-06 22:39:28 -0400

    add tests for tysig sprinting

---
+++ Test.NewLang/TySigTests.fs
@@ -0,0 +1,23 @@
+﻿module Tests.TySigTests
+
+open Xunit
+open Swensen.Unquote
+open Swensen.NewLang
+open System.Collections.Generic
+module C = Compilation
+
+[<Fact>]
+let ``non-generic`` () =
+    test <@ TySig("hi",[]).Name = "hi" @>
+
+[<Fact>]
+let ``single generic arg`` () =
+    test <@ TySig("hi",[TySig("bye",[])]).Name = "hi[bye]" @>
+
+[<Fact>]
+let ``two generic args`` () =
+    test <@ TySig("hi",[TySig("bye",[]); TySig("night",[])]).Name = "hi[bye,night]" @>
+
+[<Fact>]
+let ``nested generic args`` () =
+    test <@ TySig("hi",[TySig("bye",[TySig("night",[])]);]).Name = "hi[bye[night]]" @>
