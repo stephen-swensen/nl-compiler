081d03a9bc2d7e29981cb34a5c2ffa174385d434
stephen swensen <stephen.swensen@gmail.com>
2011-10-06 23:20:45 -0400

    tests

--- Test.NewLang/WhileLoopTests.fs
+++ Test.NewLang/WhileLoopTests.fs
@@ -20,10 +20,30 @@ let ``continue not allowed outside of while loop`` () =
 
 [<Fact>]
 let ``simple while loop`` () =
-    test <@ C.eval "x=0 in (while x<5 do x<-x+1); x " = 5 @>
+    test <@ C.eval "x=0 in (while x<5 do x<-x+1); x" = 5 @>
 
 [<Fact>]
 let ``while loop with break`` () =
     test <@ C.eval "x=0 in (while x<5 do (x<-x+1; if x == 3 then break())); x " = 3 @>
 
-//NEED TO FINISH OTHER CASES: continue, and nested while loops, invalid assignment
+[<Fact>]
+let ``return value is void so can't assign`` () =
+    raisesWith 
+        <@ C.eval "y = (x=0 in (while x<5 do x<-x+1)) in ()" @>
+        (expectedErrors [|16|])
+
+[<Fact>]
+let ``continue`` () =
+    test <@ C.eval "x=0 in y=0 in (while x<5 do (x<-x+1; if x == 3 then continue() else y<-y+1)); y " = 4 @>
+
+[<Fact>]
+let ``nested`` () =
+    test <@ C.eval "x=0 in y=0 in (while x<5 do (x<-x+1; z=0 in (while z<5 do z<-z+1; y<-y+1))); y " = 25 @>
+
+[<Fact>]
+let ``nested breaks`` () =
+    test <@ C.eval "x = 0 in (while true do ((while true do break()); break())); x" = 0 @>
+
+[<Fact>]
+let ``nested continue`` () =
+    test <@ C.eval "y=0 in (while true do ((x=0 in (while x<5 do (x<-x+1; if x == 3 then continue() else y<-y+1))); break())); y " = 4 @>
