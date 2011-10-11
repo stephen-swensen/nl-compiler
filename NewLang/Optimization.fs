ee4af9021d28723d56062756a48397c04d1f1e1a
stephen swensen <stephen.swensen@gmail.com>
2011-10-10 21:28:23 -0400

    started implementing optimization phase; not hooked up to anything yet - need to implement compiler flags and such.

---
+++ NewLang/Optimization.fs
@@ -0,0 +1,11 @@
+﻿module Swensen.NewLang.Optimization
+
+let rec optimize (exp:texp) = 
+    match exp with
+    | texp.IfThenElse(condition, thenBranch, elseBranch, ty) ->
+        let condition = optimize condition
+        match condition with
+        | texp.Bool(true) -> thenBranch
+        | texp.Bool(false) -> elseBranch
+        | _ -> texp.IfThenElse(condition, thenBranch, elseBranch, ty)
+    | _ -> exp
---
+++ Test.NewLang/OptimizationTests.fs
@@ -0,0 +1,46 @@
+﻿module Tests.OptimizationTests
+
+open Xunit
+open Swensen.Unquote
+open Swensen.NewLang
+open System.Collections.Generic
+
+module O = Optimization
+module C = Compilation
+
+[<Fact>]
+let ``unreachable else branch`` () =
+    test <@ C.parseFromString "if true then 1 else 0" |> O.optimize = C.parseFromString "1" @>
+
+[<Fact>]
+let ``unreachable then branch`` () =
+    test <@ C.parseFromString "if false then 1 else 0" |> O.optimize = C.parseFromString "0" @>
+
+[<Fact>]
+let ``unreachable else branch condition recursively optimized`` () =
+    test <@ C.parseFromString "if (if true then true else false) then 1 else 0" |> O.optimize = C.parseFromString "1" @>
+
+[<Fact>]
+let ``unreachable then branch condition recursively optimized`` () =
+    test <@ C.parseFromString "if (if false then true else false) then 1 else 0" |> O.optimize = C.parseFromString "0" @>
+
+[<Fact>]
+let ``lhs of && is false`` () =
+    test <@ C.parseFromString "false && (console.writeline('x'); false)" |> O.optimize = C.parseFromString "false" @>
+
+[<Fact>]
+let ``lhs of || is true`` () =
+    test <@ C.parseFromString "true || (console.writeline('x'); false)" |> O.optimize = C.parseFromString "true" @>
+
+[<Fact>]
+let ``recursively optimized lhs of && is false`` () =
+    test <@ C.parseFromString "(false || false) && (console.writeline('x'); false)" |> O.optimize = C.parseFromString "false" @>
+
+[<Fact>]
+let ``recursively optimized lhs of || is true`` () =
+    test <@ C.parseFromString "(true && true) || (console.writeline('x'); false)" |> O.optimize = C.parseFromString "true" @>
+
+[<Fact(Skip="first need to recursively reduce instance call sub expressions")>]
+let ``condition is optimized but doesn't result in whole if then else being optimized away`` () =
+    test <@ C.parseFromString "if ((true || true).getType() == type[boolean]) then true else false" |> O.optimize = C.parseFromString "if (true.getType() == type[boolean]) then true else false" @>
+
