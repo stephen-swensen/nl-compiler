f36333bfa9c01ffae0ecf669c7e5c72a8b4efc44
stephen swensen <stephen.swensen@gmail.com>
2011-12-02 10:39:59 -0500

    identifier type with tests

---
+++ Test.NL/IdentifierTests.fs
@@ -0,0 +1,32 @@
+﻿module Tests.IdentifierTests
+
+open Xunit
+open Swensen.Unquote
+open Swensen.NL
+open System.Collections.Generic
+open System
+module C = Compilation
+
+[<Fact>]
+let ``short`` () =
+    let ident = Ast.Identifier("short")
+    test <@ ident.Full = "short" @>
+    test <@ ident.IsLong = false @>
+    test <@ ident.LongPrefix = "" @>
+    test <@ ident.ShortSuffix = "short" @>
+
+[<Fact>]
+let ``long`` () =
+    let ident = Ast.Identifier("one.two.three.four")
+    test <@ ident.Full = "one.two.three.four" @>
+    test <@ ident.IsLong = true @>
+    test <@ ident.LongPrefix = "one.two.three" @>
+    test <@ ident.ShortSuffix = "four" @>
+
+[<Fact>]
+let ``cant be null`` () =
+    raises<System.ArgumentException> <@ Ast.Identifier(null) @>
+
+[<Fact>]
+let ``cant be empty`` () =
+    raises<System.ArgumentException> <@ Ast.Identifier("") @>
