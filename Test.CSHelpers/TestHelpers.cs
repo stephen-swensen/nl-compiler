d61cb79290ce2702807d8833cadb870d9b8e2477
stephen swensen <stephen.swensen@gmail.com>
2011-12-05 19:06:28 -0500

    Refactored "test helper" types into a C# project since it is easier to define types in C# which map closely .net IL and moreover we are able to do things like define public static fields

---
+++ Test.CSHelpers/TestHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tests
+{
+    public class Test1 {
+        public Test1() { }
+        public T DoIt<T>() { return default(T); }
+        public T DoIt2<T>(T x) { return x; }
+    }
+
+    public static class Test2<T> {
+        public static R DoIt1<R>() { return default(R); }
+    }
+}
---
+++ Test.NL/Prelude.fs
@@ -0,0 +1,18 @@
+﻿[<AutoOpen>]
+module Tests.Prelude
+
+open Swensen.NL
+
+let private csHelpersAssmLocation = typeof<Tests.Test1>.Assembly.Location
+
+///code fragment to reference this assembly within tests so we can test with types defined in this assembly
+let openAsm = sprintf "open \"%s\" in " csHelpersAssmLocation
+///code fragment to open the root namespace of this assembly for testing with types defined within this assembly
+let OpenNamespaceOrType = "open Tests in "
+///prefix used to reference this assembly and this namespace in dynamic NL tests
+let openPrefix = openAsm + OpenNamespaceOrType
+
+let expectedErrors codes = 
+    fun (e:EvaluationException) ->
+        let errors = e.Errors
+        <@ errors |> Array.map (fun err -> err.Code) = codes @>
