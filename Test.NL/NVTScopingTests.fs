616fece1b829d0a90420a256aa5f7ed54e5aebe0
stephen swensen <stephen.swensen@gmail.com>
2011-12-05 19:06:28 -0500

    Refactored "test helper" types into a C# project since it is easier to define types in C# which map closely .net IL and moreover we are able to do things like define public static fields

---
+++ Test.NL/NVTScopingTests.fs
@@ -0,0 +1,14 @@
+﻿module Tests.NVTScopingTests
+
+open Xunit
+open Swensen.Unquote
+open Swensen.NL
+open System.Collections.Generic
+open System
+module C = Compilation
+
+[<Fact>]
+let ``short`` () =
+    let ident = Ast.Identifier("short")
+    test <@ ident.Full = "short" @>
+
