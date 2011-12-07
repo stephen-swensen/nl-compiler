3341a099a77880f023d9b689e0acd65cd0f4ea78
stephen swensen <stephen.swensen@gmail.com>
2011-12-06 22:20:32 -0500

    Finished most of issue 32 (refactor TySig resolution so functional but still performant) -- but still need to do a little related refactoring.

--- NL/Utils.fs
+++ NL/Utils.fs
@@ -11,3 +11,18 @@ module Seq =
     ///cons x with xs
     let cons x xs =
         seq { yield x ; yield! xs}
+
+    let combine s1 s2 =
+        seq {
+            for x in s1 do
+                for y in s2 do
+                    yield x,y
+        }
+
+    let combine3 s1 s2 s3 =
+        seq {
+            for x in s1 do
+                for y in s2 do
+                    for z in s3 do
+                        yield x,y,z
+        }
