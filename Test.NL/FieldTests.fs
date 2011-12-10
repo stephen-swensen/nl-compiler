46e13a25b4afb4652755b6f0c2832cfc2e77cef0
stephen swensen <stephen.swensen@gmail.com>
2011-12-10 01:13:39 -0500

    fix grammer for and add test for expr dot path

--- Test.NL/FieldTests.fs
+++ Test.NL/FieldTests.fs
@@ -19,3 +19,7 @@ let ``set static field`` () =
 [<Fact>]
 let ``get static then class instance then class instance field`` () =
     test <@ C.eval (Prelude.openAsm + "Tests.Test1.sfld.ifld.ifld") = 0 @>
+
+[<Fact>]
+let ``expr get class instance then class instance field`` () =
+    test <@ C.eval (Prelude.openAsm + "Tests.Test1().ifld.ifld") = 0 @>
