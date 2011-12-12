661220695662f0c1a4eb103172902613609ca3b0
stephen swensen <stephen.swensen@gmail.com>
2011-12-11 23:05:32 -0500

    instance console logger instead of default logger

--- Nlc/Program.fs
+++ Nlc/Program.fs
@@ -6,9 +6,9 @@ let main args =
     let fileNames, asmName =
         match args with
         | [|fileNames; asmName|] -> fileNames.Split('|'), asmName // | is an invalid path name so good delimiter
-        | _ -> failwithf "Could not parse args: %A" args
+        | _ -> failwithf "Could not parse args: %A\n\nSyntax is nlc \"filename1.nl|filename2.nl|...\" \"assembly name\"" args
     try
-        Swensen.NL.ErrorLogger.InstallDefaultLogger()
+        Swensen.NL.ErrorLogger.InstallConsoleLogger()
         Compilation.compileFromFiles fileNames asmName
         0
     with e ->
