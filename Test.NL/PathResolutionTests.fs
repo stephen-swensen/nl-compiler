166a417ee5f16335b3e1fc21f93a35f956e8e95b
stephen swensen <stephen.swensen@gmail.com>
2011-12-17 09:15:44 -0500

    added more tests.
    found interesting case which is failing (set instance field of a struct which was retreived from a property): may or may not be valid

--- Test.NL/PathResolutionTests.fs
+++ Test.NL/PathResolutionTests.fs
@@ -26,3 +26,19 @@ let ``expr get class field then call method`` () =
 [<Fact>]
 let ``expr get class field then get class property then call method`` () =
     test <@ C.eval (Prelude.openAsm + "Tests.NonGenericClass1().instance_field_ngc2.instance_property_ngs1.InstanceNonGenericMethod()") = 0 @>
+
+[<Fact>]
+let ``get static then class instance then class instance field then set`` () =
+    test <@ C.eval (Prelude.openAsm + 
+                    "Tests.NonGenericClass1.static_field_ngc1.instance_field_ngc2.instance_field_int <- 3; 
+                     temp = Tests.NonGenericClass1.static_field_ngc1.instance_field_ngc2.instance_field_int in
+                     Tests.NonGenericClass1.static_field_ngc1.instance_field_ngc2.instance_field_int <- 0;
+                     temp") = 3 @>
+
+[<Fact>]
+let ``var get class instance field then class instance field then set`` () =
+    test <@ C.eval (Prelude.openAsm + "x = Tests.NonGenericClass1() in x.instance_field_ngc2.instance_field_int <- 3; x.instance_field_ngc2.instance_field_int") = 3 @>
+
+[<Fact>]
+let ``var get class property then struct property then set`` () =
+    test <@ C.eval (Prelude.openAsm + "x = Tests.NonGenericClass1() in x.instance_property_ngs1.instance_property_int <- 3; x.instance_property_ngs1.instance_property_int") = 3 @>
