8797a03e67948704b013c61c75c54f56375e1184
stephen swensen <stephen.swensen@gmail.com>
2011-12-24 12:43:09 -0500

    add lex tests

---
+++ Test.NL/LexTests.fs
@@ -0,0 +1,18 @@
+﻿///Test lex unrecognized error recovery as well as comments and newline / white space handling
+module Tests.LexTests
+
+open Xunit;; open Xunit.Extensions
+open Swensen.Unquote.Assertions
+open Swensen.NL
+open System.Collections.Generic
+open System
+open Evaluation
+
+[<Theory;EvalData>]
+let ``white space and newlines are insignificant`` options =
+    test <@ evalWith options "x        = \n 3   \r\n     in x" = evalWith options "x = 3 in x" @>
+
+[<Theory(Skip="not implemented");EvalData>]
+let ``error recovery in the presence of unrecognized charactors`` options =
+    raisesWith <@ evalWith options "x = 3 in x + y" @>
+        (expectedErrors [|39;39;5|])
