ef02c8d5d04d8fbbd3b77033cecc538f208c15f2
stephen swensen <stephen.swensen@gmail.com>
2011-12-30 17:25:23 -0500

    fix test cases having changed Nli.submit signature

--- Test.NL/NliTests.fs
+++ Test.NL/NliTests.fs
@@ -11,20 +11,20 @@ let ``single expression is equivalent to single Do statement`` options =
 
 [<Theory;NliData>]
 let ``issue 45: double semilcolon should not reduced to single semicolon in let bindings`` options =
-    test <@ Nli(options).Submit("x = 3 in x;;") = [|("it_0", 3 :> obj)|] @>
+    test <@ Nli(options).Submit("x = 3 in x;;") = [|("it0", 3 :> obj, typeof<int32>)|] @>
 
 [<Theory;NliData>]
 let ``single Do statement is bound to 0th it`` options =
-    test <@ Nli(options).Submit("3") = [|("it_0", 3 :> obj)|] @>
+    test <@ Nli(options).Submit("3") = [|("it0", 3 :> obj, typeof<int32>)|] @>
 
 [<Theory;NliData>]
 let ``let statement`` options =
-    test <@ Nli(options).Submit("x = 3;;") = [|("x", 3 :> obj)|] @>
+    test <@ Nli(options).Submit("x = 3;;") = [|("x", 3 :> obj, typeof<int32>)|] @>
 
 [<Theory;NliData>]
 let ``let and do statements intersparsed`` options =
     test <@ Nli(options).Submit("x = 1;;2;;3;;y=4;;5;;") 
-             = [|("x", 1 :> obj);("it_0", 2 :> obj);("it_1", 3 :> obj);("y", 4 :> obj);("it_2", 5 :> obj)|] @>
+             = [|("x", 1 :> obj, typeof<int32>);("it0", 2 :> obj, typeof<int32>);("it1", 3 :> obj, typeof<int32>);("y", 4 :> obj, typeof<int32>);("it2", 5 :> obj, typeof<int32>)|] @>
 
 [<Theory;NliData>]
 let ``Submit throws NliException when errors found`` options =
@@ -35,5 +35,5 @@ let ``can reference variables from previous submits`` options =
     test <@ 
             let nli = Nli(options)
             nli.Submit("x = 3;;") |> ignore 
-            nli.Submit("y = x + 2;;") = [|("y", 5 :> obj)|]
+            nli.Submit("y = x + 2;;") = [|("y", 5 :> obj, typeof<int32>)|]
          @>
