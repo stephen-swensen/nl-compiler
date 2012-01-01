ff09c1cd39b83a5971a5d65b6c8d4bcda0988694
stephen swensen <stephen.swensen@gmail.com>
2012-01-01 01:46:25 -0500

    some "refactoring"

--- VisualNli/MainForm.fs
+++ VisualNli/MainForm.fs
@@ -14,7 +14,18 @@ type public MainForm() as this =
             System.Drawing.Size((2 * size.Width) / 3, size.Height / 2)
         )
     )
+
+    //control declarations
     let nli = Swensen.NL.Nli()
+    let mutable splitc:SplitContainer = null
+    let mutable rtb:RichTextBox = null
+    let mutable treeViewPanel = null
+    let mutable treeView:WatchTreeView = Unchecked.defaultof<WatchTreeView>
+
+    //other declarations
+    let mutable textFont = new Font(FontFamily.GenericMonospace, 13.0f)
+    let errorsCount = ref 0
+    let exnCount = ref 0
 
     let render f =
         this.SuspendLayout();
@@ -25,33 +36,34 @@ type public MainForm() as this =
     do
         render <| fun () ->
             do //build the visual tree
-                let splitc = new System.Windows.Forms.SplitContainer(Dock=DockStyle.Fill, Orientation=Orientation.Horizontal, BackColor=Color.LightGray)
-
-                let rtb = new RichTextBox(Dock=DockStyle.Fill, Font=new Font(FontFamily.GenericMonospace, 14.0f))
-                splitc.Panel1.Controls.Add(rtb)
-
-                let watchp = new WatchPanel(Dock=DockStyle.Fill, BackColor=System.Drawing.SystemColors.Control)
-                splitc.Panel2.Controls.Add(watchp)
-
+                splitc <- new System.Windows.Forms.SplitContainer(Dock=DockStyle.Fill, Orientation=Orientation.Horizontal, BackColor=Color.LightGray)
+                do
+                    rtb <- new RichTextBox(Dock=DockStyle.Fill, Font=textFont)
+                    splitc.Panel1.Controls.Add(rtb)
+                do
+                    treeViewPanel <- new Panel(Dock=DockStyle.Fill, BackColor=System.Drawing.SystemColors.Control)
+                    do
+                        treeView <- new WatchTreeView(Dock=DockStyle.Fill, Font=textFont)
+                        treeViewPanel.Controls.Add(treeView)
+                    splitc.Panel2.Controls.Add(treeViewPanel)
                 this.Controls.Add(splitc)
 
-                let errorsCount = ref 0
-                let exnCount = ref 0
-                rtb.KeyDown.Add(fun args ->
+            
+            do //setup ALT+ENTER event on rich text box
+                rtb.KeyDown.Add <| fun args ->
                     if args.Alt && args.KeyCode = Keys.Enter && not <| String.IsNullOrWhiteSpace(rtb.SelectedText) then
                         try
                             match nli.TrySubmit(rtb.SelectedText) with
                             | Some(results) ->
                                 for name, value, ty in results do
-                                    watchp.Watch(name, value, ty)
+                                    treeView.Watch(name, value, ty)
                             | None ->
-                                watchp.Watch(sprintf "errors%i" !errorsCount, Swensen.NL.ErrorLogger.ActiveLogger.GetErrors())
+                                treeView.Watch(sprintf "errors%i" !errorsCount, Swensen.NL.ErrorLogger.ActiveLogger.GetErrors())
                                 errorsCount := !errorsCount + 1                             
                         with e ->
-                            watchp.Watch(sprintf "exn%i" !exnCount, e)
+                            treeView.Watch(sprintf "exn%i" !exnCount, e)
                             exnCount := !exnCount + 1
 
                         args.SuppressKeyPress <- true //so doesn't make "ping" noise
                     else 
                         ()
-                )
