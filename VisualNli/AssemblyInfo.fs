a8c7c2490229384e25c114a3c3e41a54604268bf
stephen swensen <stephen.swensen@gmail.com>
2011-12-31 20:34:44 -0500

    got the visual nli off the ground :)

---
+++ VisualNli/AssemblyInfo.fs
@@ -0,0 +1,39 @@
+﻿namespace VisualNli
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("VisualNli")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("Microsoft")>]
+[<assembly: AssemblyProduct("VisualNli")>]
+[<assembly: AssemblyCopyright("Copyright © Microsoft 2011")>]
+[<assembly: AssemblyTrademark("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("dd3ef001-1e6e-4022-9d64-39a8298e74f6")>]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+()
