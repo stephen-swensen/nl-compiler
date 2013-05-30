a30ab48f51f6468347e94175e0b13fcd034ae39c
stephen swensen <stephen.swensen@gmail.com>
2013-05-30 15:07:29 +0100

    position reporting test

--- Test.NL/LexParseAndSemantTests.fs
+++ Test.NL/LexParseAndSemantTests.fs
@@ -1,7 +1,20 @@
 ﻿module Tests.LexParseAndSemantTests
+open Swensen.NL
+open Swensen.Unquote
 open Xunit
 
 [<Fact>]
-let ``correct pos info spanning lines with default offset`` =
-    ()
+let ``correct pos info spanning lines with default offset`` () =
+    let code = """(); 4 * "asdf";;
+(); 4 * "asdf";;
+(); 4 * "asdf";;"""
+    MessageLogger.InstallInMemoryLogger()
+    Compilation.lexParseAndSemant code |> ignore
+    let msgs = MessageLogger.ActiveLogger.GetMessages()
+    let actualMsgs = msgs |> Array.map (fun msg -> msg.ToString())
+    //the important part we are looking for is that the reported line and column numbers in the ranges are correct.
+    let expectedMsgs = [|"Semantic error (NL0003) from Line 1, Column 5 to Line 1, Column 15: Binary operator '*' cannot be applied to operands of type 'Int32' and 'String'";
+                         "Semantic error (NL0003) from Line 2, Column 5 to Line 2, Column 15: Binary operator '*' cannot be applied to operands of type 'Int32' and 'String'";
+                         "Semantic error (NL0003) from Line 3, Column 5 to Line 3, Column 15: Binary operator '*' cannot be applied to operands of type 'Int32' and 'String'"|]
+    test <@ actualMsgs = expectedMsgs @>
 
