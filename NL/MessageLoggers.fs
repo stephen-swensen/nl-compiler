724a356afd91b69257eb8c5b180d51d31627fcd4
stephen swensen <stephen.swensen@gmail.com>
2013-06-09 16:22:03 +0100

    refactoring

--- NL/MessageLoggers.fs
+++ NL/MessageLoggers.fs
@@ -2,28 +2,32 @@
 
 open System
 
-type MessageLogger =
+type internal MessageLogger =
     //based on https://github.com/fsharp/fsharp/blob/master/src/fsharp/ErrorLogger.fs
     [<System.ThreadStatic; DefaultValue>]
     static val mutable private messageLogged : Event<CompilerMessage>
 
-    ///The thread static event triggered by calls to static CompilerMessage.Log
+    ///The thread static event triggered by calls to static CompilerMessage.Log. This event should typically not be
+    ///subscribed to directly, instead, use a subclass of AbstractMessageSink for automatic subscribe on init and unsubscribe
+    ///on Dispose.
     static member MessageLogged
         with get() = 
             if obj.ReferenceEquals(MessageLogger.messageLogged, null) then
                MessageLogger.messageLogged <- new Event<CompilerMessage>()
             MessageLogger.messageLogged.Publish
 
+    ///Triggers the MessageLogged event.
     static member Log(msg) =
         MessageLogger.messageLogged.Trigger(msg)
 
 ///Abstract message sink which subscribes to MessageLogger.MessageLogged on initialization and unsubscribes when disposed.
 [<AbstractClass>]
 type AbstractMessageSink() as this =
-    let logHandler = MessageLogger.MessageLogged.Subscribe(this.Log)
-    abstract Log : CompilerMessage -> unit
+    let sinkHandle = MessageLogger.MessageLogged.Subscribe(this.Sink)
+    ///Sink a log message. This method should almost never be used directly (it is invoked by the MessageLogged event).
+    abstract Sink : CompilerMessage -> unit
     interface IDisposable with
-        override __.Dispose() = logHandler.Dispose()
+        override __.Dispose() = sinkHandle.Dispose()
 
 ///A basic stateful message sink (messages are accumulated in an internal list, which can be queried / retrieved at any time)
 ///with optional console logging (default is false). This message sink is typically used for internal log orchestration.
@@ -48,8 +52,8 @@ type BasicMessageSink(?consoleLogging:bool) =
     ///Get a snapshot of all the logged errors with the given level
     member __.GetMessages(levelFilter) = messages |> Seq.filter (fun msg -> msg.Level |> levelFilter) |> Seq.toArray
 
-    ///Log the compiler error
-    override __.Log(msg:CompilerMessage) =
+    ///Sink the compiler message (this method should almost never be used directly)
+    override __.Sink(msg:CompilerMessage) =
         match msg.Level with
         | MessageLevel.Error -> errorCount <- errorCount + 1
         | MessageLevel.Warning -> warningCount <- warningCount + 1
