8ac8f61916ec161c3350b34800cfdb15b17b1b68
stephen swensen <stephen.swensen@gmail.com>
2013-10-26 18:42:31 -0400

    prelude class

---
+++ NL/NlPrelude.fs
@@ -0,0 +1,7 @@
+﻿namespace Swensen.NL
+
+///this type is opened in the default SemanticEnvironment, making all of its static members available without qualification
+///(e.g. within eval and nli, for example)
+type NlPrelude =
+    static member Eval<'a>(code:string) : 'a =
+        Evaluation.eval<'a> code
