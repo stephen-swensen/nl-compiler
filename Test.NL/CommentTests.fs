4ac8cb9a0890cd073a7f1d5ad3d8829c6ce14a25
stephen swensen <stephen.swensen@gmail.com>
2014-03-27 22:40:20 -0400

    Issue 79 single and multi line comments lex rules and tests. visualnli syntax highlighting to come

---
+++ Test.NL/CommentTests.fs
@@ -0,0 +1,37 @@
+﻿module Tests.Comments
+
+open Xunit;; open Xunit.Extensions
+open Swensen.Unquote.Assertions
+open Swensen.NL
+open System.Collections.Generic
+open System
+open Evaluation
+
+[<Fact>]
+let ``single line comment with newline and eol breaks`` () =
+    let code = """//hello //world yay!
+3
+//eol example 5"""
+    test <@ eval code = 3 @>
+
+[<Fact>]
+let ``simple multi line comment`` () =
+    let code = """/*
+3
+*/5"""
+    test <@ eval code = 5 @>
+
+[<Fact>]
+let ``eof multi line comment`` () =
+    let code = """5/*
+3
+"""
+    test <@ eval code = 5 @>
+
+[<Fact>]
+let ``nested multi line comment`` () =
+    let code = """5/*
+//hello world!
+/* 3 */
+*/"""
+    test <@ eval code = 5 @>
