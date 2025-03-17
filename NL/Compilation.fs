1f9309f9807afcfbd8f34958ecb4b6b1488313fb
Stephen Swensen <stephen.swensen@gmail.com>
2025-03-17 01:26:08 -0400

    nlc saving again

--- NL/Compilation.fs
+++ NL/Compilation.fs
@@ -1,13 +1,13 @@
 ﻿module Swensen.NL.Compilation
+open System.IO
+open System.Reflection.Metadata
+open System.Reflection.Metadata.Ecma335
+open System.Reflection.PortableExecutable
 open Swensen.NL.Ail
 
 open System
 open System.Reflection
 open System.Reflection.Emit
-
-open FSharp.Text.Lexing
-open Lexer
-open Parser
 
 module CM = CompilerMessages
 module SA = SemanticAnalysis
@@ -17,23 +17,40 @@ module SA = SemanticAnalysis
 ///ast -> assemblyName -> unit
 let compileFromAil (ail:ILExpr) (asmName:string) =
     ()
-    //TODO: need to wait for .net 8 for Save support - https://github.com/dotnet/runtime/issues/62956
 
-    //let asmFileName = asmName + ".exe"
-    //let asmBuilder = AssemblyBuilder.DefineDynamicAssembly(AssemblyName(Name=asmName), AssemblyBuilderAccess.RunAndSave)
-    //let modBuilder = asmBuilder.DefineDynamicModule(asmName + ".netmodule", asmFileName, false) //need to specify asmFileName here!
-    //let tyBuilder = modBuilder.DefineType(asmName + ".Application", TypeAttributes.Public)
-    //let methBuilder = tyBuilder.DefineMethod("Run", MethodAttributes.Public ||| MethodAttributes.Static, typeof<System.Void>, null)
+    let asmFileName = asmName + ".exe"
+    let asmBuilder = PersistedAssemblyBuilder(AssemblyName(asmName), typeof<obj>.Assembly);
+    let modBuilder = asmBuilder.DefineDynamicModule(asmName + ".netmodule")
+    let tyBuilder = modBuilder.DefineType(asmName + ".Application", TypeAttributes.Public)
+    let methBuilder = tyBuilder.DefineMethod("Run", MethodAttributes.Public ||| MethodAttributes.Static, typeof<System.Void>, null)
 
-    //let ilExpr = ail
+    let ilExpr = ail
 
-    //let il = methBuilder.GetILGenerator() |> SmartILGenerator.fromILGenerator
-    //Emission.emit false il ilExpr
-    //il.Emit(OpCodes.Ret)
+    let il = methBuilder.GetILGenerator() |> SmartILGenerator.fromILGenerator
+    Emission.emit false il ilExpr
+    il.Emit(OpCodes.Ret)
 
-    //tyBuilder.CreateType() |> ignore
-    //asmBuilder.SetEntryPoint(methBuilder, PEFileKinds.ConsoleApplication)
-    //asmBuilder.Save(asmFileName)
+    let ty = tyBuilder.CreateType()
+
+    let metadataBuilder, ilStream, fieldData =
+        let mutable ilStreamOut = BlobBuilder()
+        let mutable fieldDataOut = BlobBuilder()
+        let mb = asmBuilder.GenerateMetadata(&ilStreamOut, &fieldDataOut)
+        mb, ilStreamOut, fieldDataOut
+
+    let peBuilder =
+        ManagedPEBuilder(
+            header = PEHeaderBuilder.CreateExecutableHeader(),
+            metadataRootBuilder = new MetadataRootBuilder(metadataBuilder),
+            ilStream = ilStream,
+            mappedFieldData = fieldData,
+            entryPoint = MetadataTokens.MethodDefinitionHandle(methBuilder.MetadataToken))
+
+    let peBlob = BlobBuilder()
+    peBuilder.Serialize(peBlob) |> ignore
+
+    use fileStream = new FileStream(asmFileName, FileMode.Create, FileAccess.Write)
+    peBlob.WriteContentTo(fileStream)
 
 ///Compile the given source code string into an assembly
 ///code -> assemblyName -> unit
@@ -44,7 +61,7 @@ let compileFromString =
 ///fileNames -> assemblyName -> unit
 let compileFromFiles fileNames =
     fileNames
-    |> Seq.map System.IO.File.ReadAllText
-    |> String.concat System.Environment.NewLine
+    |> Seq.map File.ReadAllText
+    |> String.concat Environment.NewLine
     //|> (fun text -> printfn "%s" text; text)
     |> compileFromString
